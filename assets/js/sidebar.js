c3629c6d8a66c2c8c646a66285afa49de0ac1cfb
Huỳnh Dũng Hồ <hohuynhdung@gmail.com>
2021-07-19 10:59:44 +0700

    test

---
+++ assets/js/sidebar.js
@@ -0,0 +1,7 @@
+
+        $(document).ready(function(){
+            $(".bar").click(function(){
+            $(".merchant__navbar").toggleClass("hide")
+            $("header , .merchant__management-order").toggleClass("slide-left")
+        })
+        });
